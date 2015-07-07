17743afd54fed4469ee81c74fcb3a51114f5d95e
Carter De Angelis <me@carterdea.com>
2015-07-07 14:26:36 -0700

    Finally got stripe charges working, but it will need  alot of improvement & refactoring

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -12,22 +12,13 @@ class DonationsController < ApplicationController
   end
 
   def create
-    if params[:family_id].present?
-      @family = Family.find(params[:family_id])
-      @donation = @family.donations.build(donation_params)
-      if @donation.save
-        redirect_to @family, notice: "Thanks so much for your generous donation!"
-      else
-        render :new
-      end
+    @family = Family.find(params[:family_id])
+    @donation = @family.donations.build(donation_params)
+    @stripe_token = :stripe_token
+    if @donation.create_stripe_charge && @donation.save
+      redirect_to @family, notice: "Thanks so much for your generous donation!"
     else
-      @family = nil
-      @donation = Donation.new(donation_params)
-      if @donation.save
-        redirect_to root_url, notice: "Thanks so much for your generous donation!"
-      else
-        render :new
-      end
+      render :new
     end
   end
 
@@ -41,7 +32,20 @@ class DonationsController < ApplicationController
 
 private
   def donation_params
-    params.require(:donation).permit(:family_id, :amount, :at_tip, :name, :anonymous, :message, :email, :at_newsletter, :family_email_updates, :hide_amount, :recurring)
+    params.require(:donation).permit(
+      :family_id,
+      :amount,
+      :at_tip,
+      :name,
+      :anonymous,
+      :message,
+      :email,
+      :at_newsletter,
+      :family_email_updates,
+      :hide_amount,
+      :recurring,
+      :stripe_token
+    )
   end
 
 end
--- app/models/donation.rb
+++ app/models/donation.rb
@@ -9,7 +9,7 @@ class Donation < ActiveRecord::Base
   validates_numericality_of :amount, greater_than_or_equal_to: 100, message: "Must be at least $1."
   # validates_uniqueness_of :uuid
 
-  attr_accessor :stripe_customer_id, :stripe_subscription_id, :stripe_token
+  attr_accessor :stripe_token
 
   DONATION_AMOUNTS = [
     30, 50, 100, 500, 1000
@@ -19,39 +19,57 @@ class Donation < ActiveRecord::Base
     0, 10, 20
   ]
 
-def donor_name
-  if self.anonymous.false?
-    self.name
-  else
-    "Anonymous"
-  end
-end
-
-def stripe_description
-  "AdoptTogether Donation"
-end
-
-def create_stripe_charge
-end
-
-def create_stripe_subcsription
-end
-
-def update_stripe_customer_id
-end
-
-def send_receipt
-end
-
-def stripe_subscription
-end
-
-def populate_uuid
-  if new_record?
-    while !valid? || self.uuid.nil?
-      self.uuid = SecureRandom.hex(1_000_000_000).to_s(36)
+  def donor_name
+    if self.anonymous.false?
+      self.name
+    else
+      "Anonymous"
     end
   end
-end
+
+  def tip_total
+    tip_percent = at_tip / 100
+    amount * tip_percent
+  end
+
+  def total_amount_in_cents
+    amount += tip_total
+    amount.to_i * 100
+  end
+
+  def stripe_description
+    "AdoptTogether Donation"
+  end
+
+  def create_stripe_charge
+    if valid?
+      # Get the credit card details submitted by the form
+      token = self.stripe_token
+
+      # Create the charge on Stripe's servers - this will charge the user's card
+      begin
+        charge = Stripe::Charge.create(
+          :amount => total_amount_in_cents, # amount in cents, again
+          :currency => "usd",
+          :source => token,
+          :description => stripe_description
+        )
+      rescue Stripe::CardError => e
+        # The card has been declined
+      end
+    end
+  end
+
+  def create_stripe_subcsription
+  end
+
+  def update_stripe_customer_id
+  end
+
+  def send_receipt
+  end
+
+  def stripe_subscription
+  end
 
 end
