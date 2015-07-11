d7e6e4f5d0b0272e33e7a2a7a770b2966d97e1ad
Carter De Angelis <me@carterdea.com>
2015-07-10 18:43:18 -0700

    Adding the region to carrierwave to see if that fixes Heroku

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,7 +4,8 @@ if Rails.env.production?
       # Configuration for Amazon S3
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+      :region                => 'us-west-2'
     }
     config.fog_directory     =  ENV['S3_BUCKET']
   end
