4849febde46b15d41c0821687c035ac6a7c1bc58
Carter De Angelis <carterdea@gmail.com>
2015-08-31 21:33:00 -0700

    Fixing application.js sprockets issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,8 +2,8 @@
 //= require jquery.turbolinks
 //= require jquery_ujs
 //= require parsley
-// = require_tree ../../../vendor/assets/javascripts
-// = require_tree ../../../vendor/assets/bower_components
+//= require_tree ../../../vendor/assets/javascripts
+//= require_tree ../../../vendor/assets/bower_components
 //= require_tree .
 //= require bootstrap
 //= require turbolinks
