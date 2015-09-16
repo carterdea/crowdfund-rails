52cf90779a9aedf08182eb43d3f895729046f79e
Carter De Angelis <carterdea@gmail.com>
2015-09-16 03:53:20 -0700

    Deleting placeholder specs. Will add real specs for the applicable controllers, views, etc

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,21 @@
 module ApplicationHelper
-	def body_class
-		"#{controller_name} #{action_name}"
-	end
+  def body_class
+    "#{controller_name} #{action_name}"
+  end
 
-  def title(title=nil)
+  def title(title = nil)
     if title.present?
       content_for :title, title
     else
-      content_for?(:title) ? content_for(:title) + ' | AdoptTogether'  : 'AdoptTogether | Helping Families Fund Adoption'
+      content_for?(:title) ? content_for(:title) + ' | AdoptTogether' : 'AdoptTogether | Helping Families Fund Adoption'
     end
   end
 
-  def meta_description(description=nil)
+  def meta_description(description = nil)
     if description.present?
       content_for :meta_description, description
     else
-      content_for?(:meta_description) ? content_for(:meta_description)  : 'AdoptTogether is a non-profit funding platform helping families raise money to pay for adoption costs.'
+      content_for?(:meta_description) ? content_for(:meta_description) : 'AdoptTogether is a non-profit funding platform helping families raise money to pay for adoption costs.'
     end
   end
 
@@ -26,6 +26,6 @@ module ApplicationHelper
   def share_count(url)
     SocialShares.total url, %w(facebook twitter googleplus linkedin pinterest)
   rescue
-  	0
+    0
   end
 end
--- spec/views/password_resets/edit.html.erb_spec.rb
+++ spec/views/password_resets/edit.html.erb_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe "password_resets/edit.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
 end
