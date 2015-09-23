e83db28fc2eeb24d04259d8b1c7db0a254aee3ed
Carter De Angelis <carterdea@gmail.com>
2015-09-23 00:18:59 -0700

    Fixing a busted ENV variable in production.rb and making Adoption Agency URL not required via Parsely

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,21 +63,22 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = fals
-  
+
   config.action_mailer.smtp_settings = {
-    address:        'smtp.mandrillapp.com',
-    port:           587,
-    user_name:      ENV['MANDRILL_USERNAME'],
-    password:       ENV['MANDRILL_APIKEY'],
-    domain:         'heroku.com',
-    authentication: :plain
+    address:                'smtp.mandrillapp.com',
+    port:                   587,
+    enable_starttls_auto:   true,
+    user_name:              ENV['MANDRILL_USERNAME'],
+    password:               ENV['MANDRILL_API_KEY'],
+    authentication:         'plain',
+    domain:                 'heroku.com'
   }
 
   config.action_mailer.default_url_options = { host: 'adopttogether.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default charset: 'utf-8'
-  
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
