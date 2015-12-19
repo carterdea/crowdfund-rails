696eeb066e71789d86a67eec7029deb96b3f1615
Carter De Angelis <carterdea@gmail.com>
2015-12-18 18:56:01 -0800

    Fixing small cacheing bug that made the current_user donate button show for other users, making small changes to forms for mobile 📱 (according to baymard)

--- app/helpers/updates_helper.rb
+++ app/helpers/updates_helper.rb
@@ -3,7 +3,7 @@ module UpdatesHelper
     OEmbed::Providers.register(OEmbed::Providers::Youtube, OEmbed::Providers::Vimeo)
     resource = OEmbed::Providers.get(video_url) #=> OEmbed::Response
     resource.html.html_safe
-  # rescue
-  #   "We're having trouble parsing your URL. Can you try again?"
+  rescue
+    "We're having trouble parsing your URL. Can you try again?"
   end
 end
