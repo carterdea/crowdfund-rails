9899a4a117e246d3a276449f1f3994c9416d498e
Carter De Angelis <carterdea@gmail.com>
2015-12-23 00:27:49 -0800

    [fu] Adding caching back in & adding a bit of exception handling

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,7 +1,7 @@
 class Page < ActiveRecord::Base
   def self.tumblr_posts
-    # Rails.cache.fetch('last_3_tumblr_posts', expires_in: 1.hour) do
-    Tumblr::Client.new.posts('adopttogether.tumblr.com', limit: 3)['posts']
-    # end
+    Rails.cache.fetch('last_3_tumblr_posts', expires_in: 1.hour) do
+      Tumblr::Client.new.posts('adopttogether.tumblr.com', limit: 3)['posts']
+    end
   end
 end
