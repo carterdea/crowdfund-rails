54c1f17669438973528f830713edce62916dc92a
Carter De Angelis <carterdea@gmail.com>
2015-12-22 18:51:03 -0800

    [fu] Blog Post cacheing

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,8 +1,7 @@
 class Page < ActiveRecord::Base
   def self.tumblr_posts
     Rails.cache.fetch('last_3_tumblr_posts', expires_in: 1.hour) do
-      client = Tumblr::Client.new
-      posts_json = client.posts('adopttogether.tumblr.com', limit: 3)['posts']
+      Tumblr::Client.new.posts('adopttogether.tumblr.com', limit: 3)['posts']
     end
   end
 end
