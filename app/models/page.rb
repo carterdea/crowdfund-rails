a8e669caf74a37743ae948244eb3ebc0c42d8b12
Carter De Angelis <carterdea@gmail.com>
2015-12-22 18:33:33 -0800

    Fix a few issues w/ cacheing on Family Show & Home

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,9 +1,8 @@
 class Page < ActiveRecord::Base
   def self.tumblr_posts
-    Rails.cache.fetch('last_3_tumblr_posts', expires_in: 12.hours) do
+    Rails.cache.fetch('last_3_tumblr_posts', expires_in: 1.hour) do
       client = Tumblr::Client.new
       posts_json = client.posts('adopttogether.tumblr.com', limit: 3)['posts']
-      posts = posts_json
     end
   end
 end
