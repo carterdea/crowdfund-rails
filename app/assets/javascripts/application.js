bd81a4ed12b3a9f99043c754b78c9aab8b0635f1
Carter De Angelis <carterdea@gmail.com>
2015-12-23 22:35:09 -0800

    Get rid of retina_tag and make all images @2x, put Mailcheck.js into Bower

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,13 +1,13 @@
 //= require jquery2
 //= require jquery.turbolinks
 //= require jquery_ujs
-//= require retina_tag
 
 // Bower packages
 //= require raphael/raphael
 //= require jquery-mask-plugin/src/jquery.mask
 //= require morris.js/morris
 //= require parsleyjs/dist/parsley
+//= require mailcheck/src/mailcheck.min.js
 //= require jquery.payment/lib/jquery.payment
 //= require jquery.collapser.min.js/index
 //= require jquery.fitvids/jquery.fitvids
