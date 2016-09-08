c8f584ce30007b721a844395903f763bcf1f24be
Carter De Angelis <carterdea@gmail.com>
2016-09-07 17:02:35 -0700

    Airbrake stop reporting on dev & test

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,4 +1,7 @@
 Airbrake.configure do |config|
   config.project_key = ENV['AIRBRAKE_PROJECT_KEY']
   config.project_id = ENV['AIRBRAKE_PROJECT_ID']
+  config.environment = Rails.env
+  config.ignore_environments = %w(development test)
+
 end
